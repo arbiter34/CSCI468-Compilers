80bdd70fd6094a8aded1a1c66c6a3198e7db6f37
Shane Israel <shanemisrael@gmail.com>
2015-02-06 10:27:13 -0700

    Added getters and setters

--- MicroPascalCompiler/src/micropascalcompiler/TokenContainer.java
+++ MicroPascalCompiler/src/micropascalcompiler/TokenContainer.java
@@ -11,15 +11,15 @@ package micropascalcompiler;
  */
 public class TokenContainer {
     private TokenType token;
-    private int error_number;
+    private int errorNumber;
     private int col;
     private int row;
     private int length;
     private boolean isError;
-    
-    public TokenContainer(TokenType token, int error_number, int row, int col, int length, boolean isError) {
+
+	public TokenContainer(TokenType token, int errorNumber, int row, int col, int length, boolean isError) {
         this.token = token;
-        this.error_number = error_number;
+        this.errorNumber = errorNumber;
         this.col = col;
         this.row = row;
         this.length = length;
@@ -34,19 +34,39 @@ public class TokenContainer {
         this.token = token;
     }
     
-    public int getErrorNumber() {
-        return this.error_number;
+    public void setErrorNumber(int errorNumber) {
+        this.errorNumber = errorNumber;
     }
     
-    public void setErrorNumber(int error_number) {
-        this.error_number = error_number;
-    }
-    
-    public boolean isError() {
-        return this.isError;
-    }
-    
-    public int getLength() {
-        return this.length;
-    }
+    public int getCol() {
+		return col;
+	}
+
+	public void setCol(int col) {
+		this.col = col;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getErrorNumber() {
+		return errorNumber;
+	}
+
+	public void setLength(int length) {
+		this.length = length;
+	}
+	
+	public int getLength() {
+		return this.length;
+	}
+
+	public void setError(boolean isError) {
+		this.isError = isError;
+	}
 }
