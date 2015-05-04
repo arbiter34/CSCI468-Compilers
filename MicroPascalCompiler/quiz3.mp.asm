366a8671e05082069963ad2f0d12844cd7789411
Shane Israel <shanemisrael@gmail.com>
2015-05-04 12:20:31 -0600

    My version converted question 4 to pascal

--- MicroPascalCompiler/quiz3.mp.asm
+++ MicroPascalCompiler/quiz3.mp.asm
@@ -5,13 +5,16 @@ add SP #1 SP
 mov D1 -5(SP)
 sub SP #5 D1
 	; activation end
-rd 4(D1)
+push #10
+pop 4(D1)
 push 4(D1)
 push 1(D0)
 push 1(D1)
 muls
 adds
 pop 1(D0)
+push 4(D1)
+pop 1(D1)
 push 1(D0)
 castsf
 push 2(D1)
@@ -28,18 +31,33 @@ L1:
 add SP #4 SP
 sub SP #4 D0
 	; activation end
-rd 0(D0)
-rd 1(D0)
-rdf 2(D0)
+push #5
+pop 0(D0)
+push #6
+pop 1(D0)
+push #5.5
+pop 2(D0)
 add SP #1 SP
 push 0(D0)
 push 2(D0)
 call L2
 sub SP #2 SP
 sub SP #1 SP
+push #"a = "
+wrts
+push #"\n"
+wrts
 push 0(D0)
 wrts
+push #"b = "
+wrts
+push #"\n"
+wrts
 push 1(D0)
+wrts
+push #"g = "
+wrts
+push #"\n"
 wrts
 push 2(D0)
 wrts
